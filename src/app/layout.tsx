3bf69353053ad49a9566db95d4ffdcd87d7339e1
ttNKss <work.ttnkss@gmail.com>
2024-10-12 05:17:09 +0000

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
+import { Noto_Sans_JP } from 'next/font/google'
 import './globals.css'
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
 })
 
 export const metadata: Metadata = {
@@ -24,12 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ja" className={`${notoSansJP.className}`}>
+      <body>{children}</body>
     </html>
   )
 }
