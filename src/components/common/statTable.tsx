9d023072b37defde964aa9b03d1fd0e3237ac4ea
𝗧𝗞𝗦 <133070003+ttNKss@users.noreply.github.com>
2024-10-13 15:59:16 +0900

    v1.1.0 (#3)
    * extension: github actions
    
    * 最終試験前バージョン
    
    * accordionの外をクリックしたときに閉じるように変更
    
    * スクロール挙動変更
    
    * deploy実行条件修正

--- src/components/common/statTable.tsx
+++ src/components/common/statTable.tsx
@@ -8,12 +8,12 @@ import {
 } from '@/components/ui/table'
 
 interface StatTableProps {
-  last: { Vo: number; Da: number; Vi: number }
+  last?: { Vo: number; Da: number; Vi: number }
   result: { Vo: number; Da: number; Vi: number }
 }
 
 const StatTable: React.FC<StatTableProps> = ({
-  last: { Vo, Da, Vi },
+  last,
   result: { Vo: Vo_result, Da: Da_result, Vi: Vi_result }
 }) => {
   return (
@@ -21,29 +21,29 @@ const StatTable: React.FC<StatTableProps> = ({
       <TableHeader>
         <TableRow>
           <TableHead>ステータス</TableHead>
-          <TableHead>最終試験前</TableHead>
+          {last && <TableHead>最終試験前</TableHead>}
           <TableHead>最終試験後</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         <TableRow>
           <TableCell>Vo</TableCell>
-          <TableCell>{Vo}</TableCell>
+          {last && <TableCell>{last.Vo}</TableCell>}
           <TableCell>{Vo_result}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell>Da</TableCell>
-          <TableCell>{Da}</TableCell>
+          {last && <TableCell>{last.Da}</TableCell>}
           <TableCell>{Da_result}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell>Vi</TableCell>
-          <TableCell>{Vi}</TableCell>
+          {last && <TableCell>{last.Vi}</TableCell>}
           <TableCell>{Vi_result}</TableCell>
         </TableRow>
         <TableRow>
           <TableCell>合計</TableCell>
-          <TableCell>{Vo + Da + Vi}</TableCell>
+          {last && <TableCell>{last.Vo + last.Da + last.Vi}</TableCell>}
           <TableCell>{Vo_result + Da_result + Vi_result}</TableCell>
         </TableRow>
       </TableBody>
